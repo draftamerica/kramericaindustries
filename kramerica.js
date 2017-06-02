357553ec45f2c5b1eaa56b9d2d9ecc08285204d4
draftamerica <steelax42@gmail.com>
2017-06-02 14:45:42 -0400

    initial commit

---
+++ kramerica.js
@@ -0,0 +1,48 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+window.onload = function() {
+     setTimeout(KramerModal, 3000);
+}
+
+
+function KramerModal() {
+    console.log("==KramerModal==");
+    var thisModal = document.getElementById("myModal");
+    thisModal.style.visibility = "visible";
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//====ARCHIVE======//
+
+// $(document).ready(function () {
+//        $('#button').click(function (e) { // Button which will activate our modal
+//            $('#modal').reveal({ // The item which will be opened with reveal
+//                animation: 'fade',                   // fade, fadeAndPop, none
+//                animationspeed: 600,                       // how fast animtions are
+//                closeonbackgroundclick: true,              // if you click background will modal close?
+//                dismissmodalclass: 'close'    // the class of a button or element that will close an open modal
+//            });
+//            return false;
+//        });
+//    });
