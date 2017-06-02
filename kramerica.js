bc96e79c712a04670d30d01ba4378a16cbf7ddb2
draftamerica <steelax42@gmail.com>
2017-06-02 15:18:47 -0400

    added media queries

--- kramerica.js
+++ kramerica.js
@@ -13,7 +13,6 @@ function KramerModal() {
 }
 
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 
@@ -21,7 +20,6 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
